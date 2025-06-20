8d80242a61cb5fe0e36b9234a021fdf40ce4bb14
kozlov-dmit <kozlov.dmit@gmail.com>
2025-06-20 10:36:04 +0300

    Add basic unit tests

---
+++ src/test/java/com/example/migrator/ConfigTest.java
@@ -0,0 +1,35 @@
+package com.example.migrator;
+
+import org.junit.jupiter.api.Test;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.Properties;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ConfigTest {
+    @Test
+    void fromLoadsIdsFromFile() throws Exception {
+        Path tmp = Files.createTempFile("ids", ".txt");
+        Files.write(tmp, List.of("id1", "", "id2"));
+
+        Properties props = new Properties();
+        props.setProperty("source.url", "surl");
+        props.setProperty("source.user", "suser");
+        props.setProperty("source.password", "spass");
+        props.setProperty("target.url", "turl");
+        props.setProperty("target.user", "tuser");
+        props.setProperty("target.password", "tpass");
+        props.setProperty("batchSize", "50");
+        props.setProperty("taskName", "task1");
+        props.setProperty("idsFile", tmp.toString());
+
+        Config cfg = Config.from(props);
+        assertEquals("surl", cfg.sourceUrl());
+        assertEquals("tuser", cfg.targetUser());
+        assertEquals(50, cfg.batchSize());
+        assertEquals(List.of("id1", "id2"), cfg.ids());
+    }
+}
